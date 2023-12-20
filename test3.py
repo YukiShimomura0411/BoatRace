46d425f4efa408000198836097af581902d6adb1
YukiShimomura0411 <2120577h@stu.kobe-u.ac.jp>
2023-12-20 16:06:34 +0900

    エラーが出る

--- test3.py
+++ test3.py
@@ -13,10 +13,8 @@ from lhafile import LhaFile
 from pandas._testing import get_dtype
 
 
+# 圧縮ファイルをウェブからダウンロードし解凍 >> テキストファイルを保存
 def download_file(obj, date):
-    """
-    obj (str): 'racelists' or 'results'
-    """
     date = str(pd.to_datetime(date).date())
     ymd = date.replace('-', '')
     S, s = ('K', 'k') if obj == 'results' else ('B', 'b')
@@ -28,15 +26,15 @@ def download_file(obj, date):
             url_t = f'http://www1.mbrace.or.jp/od2/{S}/'
             url_b = f'{ymd[:-2]}/{s}{ymd[2:]}.lzh'
             wget.download(url_t + url_b, f'downloads/{obj}/{ymd}.lzh')
-            with LhaFile(f'downloads/{obj}/{ymd}.lzh') as archive:
-                d = archive.read(archive.infolist()[0].filename)
-                with open(f'downloads/{obj}/{ymd}.txt', 'wb') as f:
-                    f.write(d)
+            archive = LhaFile(f'downloads/{obj}/{ymd}.lzh')
+            d = archive.read(archive.infolist()[0].filename)
+            with open(f'downloads/{obj}/{ymd}.txt', 'wb') as file:
+                file.write(d)
             os.remove(f'downloads/{obj}/{ymd}.lzh')
         except urllib.request.HTTPError:
             print(f'There are no data for {date}')
 
-download_file('racelists','2022-12-04')
+download_file('racelists', '2023-11-16')
 
 
 
