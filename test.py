02c09baa111cbff9cc545357bfc5c2f9c8cddcb6
YukiShimomura0411 <116633628+YukiShimomura0411@users.noreply.github.com>
2025-01-06 15:28:00 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,215 +1,225 @@
-# 圧縮ファイルをウェブからダウンロードし解凍 >> テキストファイルを保存
-import csv
-import os
-import subprocess
-import urllib
-import re
-from datetime import date, timedelta
-
-import mojimoji as mojimoji
 import numpy as np
 import pandas as pd
-import wget
-from Tools.scripts.dutree import display
-from lhafile import LhaFile
-from pandas._testing import get_dtype
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+import torch
+import torch.nn as nn
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
 
-def date_range(start, stop, step=timedelta(days=1)):
-    current = start
-    while current <= stop:
-        yield current
-        current += step
+data = pd.read_csv('/content/drive/MyDrive/merge_3/卒論データ.csv', index_col = None, header = 0, encoding = "cp932")
 
-# 圧縮ファイルをウェブからダウンロードし解凍 >> テキストファイルを保存
-def download_file(obj, date):
-    date = str(pd.to_datetime(date).date())
-    ymd = date.replace('-', '')
-    S, s = ('K', 'k') if obj == 'results' else ('B', 'b')
-    if os.path.exists(f'D:/BoatRace/result_txt/{obj}/{ymd}.txt'):
-        return
-    else:
-        os.makedirs(f'D:/BoatRace/result_txt/{obj}', exist_ok=True)
-        try:
-            url_t = f'http://www1.mbrace.or.jp/od2/{S}/'
-            url_b = f'{ymd[:-2]}/{s}{ymd[2:]}.lzh'
-            wget.download(url_t + url_b, f'D:/BoatRace/result_txt/{obj}/{ymd}.lzh')
-            archive = LhaFile(f'D:/BoatRace/result_txt/{obj}/{ymd}.lzh')
-            d = archive.read(archive.infolist()[0].filename)
-            with open(f'D:/BoatRace/result_txt/{obj}/{ymd}.txt', 'wb') as file:
-                file.write(d)
-            archive = None
-            os.remove(f'D:/BoatRace/result_txt/{obj}/{ymd}.lzh')
-        except urllib.request.HTTPError:
-            print(f'There are no data for {date}')
+X = data.drop(["win_1"], axis=1)
+T = data[["win_1"]]
 
-def read_file(obj, date):
-    """
-    obj (str): 'racelists' or 'results'
-    """
-    date = str(pd.to_datetime(date).date())
-    ymd = date.replace('-', '')
-    f = open(f'D:/BoatRace/result_txt/{obj}/{ymd}.txt', 'r', encoding='cp932')
-    Lines = [l.strip().replace('\u3000', '') for l in f]
-    Lines = [mojimoji.zen_to_han(l, kana=False) for l in Lines][1:-1]
-    lines_by_plc = {}
-    for l in Lines:
-        if 'BGN' in l:
-            place_cd = int(l[:-4])
-            lines = []
-        elif 'END' in l:
-            lines_by_plc[place_cd] = lines
-        else:
-            lines.append(l)
-    return lines_by_plc
+X_train, X_test, T_train, T_test = train_test_split(X, T, test_size=0.1, random_state = 42)
 
-place_mapper = {
-    1: '桐生', 2: '戸田', 3: '江戸川', 4: '平和島', 5: '多摩川',
-    6: '浜名湖', 7: '蒲郡', 8: '常滑', 9: '津', 10: '三国',
-    11: '琵琶湖', 12: '住之江', 13: '尼崎', 14: '鳴門', 15: '丸亀',
-    16: '児島', 17: '宮島', 18: '徳山', 19: '下関', 20: '若松',
-    21: '芦屋', 22: '福岡', 23: '唐津', 24: '大村'
-}
-def get_results(date):
-    conv_racetime = lambda x: np.nan if x == '.' else\
-        sum([w * float(v) for w, v in zip((60, 1, 1/10), x.split('.'))])
-    info_cols = ['title', 'day', 'date', 'place_cd', 'place']
-    race_cols = ['race_no', 'race_type', 'distance', 'weather', 'wind', 'wind_strength', 'wave']
-    keys = ['toban', 'name', 'moter_no', 'boat_no',
-            'ET', 'SC', 'ST', 'RT', 'position']
-    racer_cols = [f'{k}_{i}' for k in keys for i in range(1, 7)]
-    res_cols = []
-    for k in ('tkt', 'odds', 'poprank'):
-        for type_ in ('1t', '1f1', '1f2', '2t', '2f',
-                      'w1', 'w2', 'w3', '3t', '3f'):
-            if (k == 'poprank') & (type_ in ('1t', '1f1', '1f2')):
-                pass
-            else:
-                res_cols.append(f'{k}_{type_}')
-    res_cols.append('win_method')
-    cols = info_cols + race_cols + racer_cols + res_cols
+DataList = []
+for i in range(66):
+    DataList.append(f'PC{i+1}')
 
-    stack = []
-    date = str(pd.to_datetime(date).date())
-    for place_cd, lines in read_file('results', date).items():
-        min_lines = 26
-        if len(lines) < min_lines:
-            continue
-        title = lines[4]
-        day = int(re.findall('第(\d)日', lines[6].replace(' ', ''))[0])
-        place = place_mapper[place_cd]
-        info = {k: v for k, v in zip(
-            info_cols, [title, day, date, place_cd, place])}
+x_train = X_train[DataList]
+t_train = T_train["win_1"]
 
-        head_list = []
-        race_no = 1
-        for i, l in enumerate(lines[min_lines:]):
-            if f'{race_no}R' in l:
-                head_list.append(min_lines + i)
-                race_no += 1
-        for race_no, head in enumerate(head_list, 1):
-            try:
-                race_type = lines[head].split()[1]
-                distance = int(re.findall('H(\d*)m', lines[head])[0])
-                win_method = lines[head + 1].split()[-1]
-                if "進入固定" in lines[head]:
-                    weather = lines[head].split()[4]
-                else:
-                    weather = lines[head].split()[3]
-                if "進入固定" in lines[head]:
-                    wind = lines[head].split()[6]
-                else:
-                    wind = lines[head].split()[5]
-                if "進入固定" in lines[head]:
-                    wind_strength = lines[head].split()[7]
-                else:
-                    wind_strength = lines[head].split()[6]
-                if "進入固定" in lines[head]:
-                    wave = lines[head].split()[9]
-                else:
-                    wave = lines[head].split()[8]
-                _, tkt_1t, pb_1t = lines[head + 10].split()
-                _, tkt_1f1, pb_1f1, tkt_1f2, pb_1f2 = lines[head + 11].split()
-                _, tkt_2t, pb_2t, _, pr_2t = lines[head + 12].split()
-                _, tkt_2f, pb_2f, _, pr_2f = lines[head + 13].split()
-                _, tkt_w1, pb_w1, _, pr_w1 = lines[head + 14].split()
-                tkt_w2, pb_w2, _, pr_w2 = lines[head + 15].split()
-                tkt_w3, pb_w3, _, pr_w3 = lines[head + 16].split()
-                _, tkt_3t, pb_3t, _, pr_3t = lines[head + 17].split()
-                _, tkt_3f, pb_3f, _, pr_3f = lines[head + 18].split()
-                race_vals = [race_no, race_type, distance, weather, wind, wind_strength, wave]
-                res_vals = [
-                    tkt_1t, tkt_1f1, tkt_1f2, tkt_2t, tkt_2f,
-                    tkt_w1, tkt_w2, tkt_w3, tkt_3t, tkt_3f,
-                    pb_1t, pb_1f1, pb_1f2, pb_2t, pb_2f,
-                    pb_w1, pb_w2, pb_w3, pb_3t, pb_3f,
-                    pr_2t, pr_2f, pr_w1, pr_w2, pr_w3,
-                    pr_3t, pr_3f, win_method
-                ]
-                dic = info.copy()
-                dic.update(dict(zip(race_cols, race_vals)))
-                dic.update(dict(zip(res_cols, res_vals)))
-                dic = {k: float(v) / 100 if 'odds' in k else v
-                       for k, v in dic.items()}
-                for i in range(6):
-                    bno, *vals = lines[head + 3 + i].split()[1:10]
-                    vals.append(i + 1)
-                    keys = ['toban', 'name', 'moter_no', 'boat_no',
-                            'ET', 'SC', 'ST', 'RT', 'position']
-                    dic.update(zip([f'{k}_{bno}' for k in keys], vals))
-                stack.append(dic)
-            except IndexError:
-                continue
-            except ValueError:
-                continue
-    if len(stack) > 0:
-        df = pd.DataFrame(stack)[cols].dropna(how='all')
-        df['tkt_2t'] = df['tkt_2t'].apply(lambda x: f"'{x}")
-        df['tkt_2f'] = df['tkt_2f'].apply(lambda x: f"'{x}")
-        df['tkt_w1'] = df['tkt_w1'].apply(lambda x: f"'{x}")
-        df['tkt_w2'] = df['tkt_w2'].apply(lambda x: f"'{x}")
-        df['tkt_w3'] = df['tkt_w3'].apply(lambda x: f"'{x}")
-        df['tkt_3t'] = df['tkt_3t'].apply(lambda x: f"'{x}")
-        df['tkt_3f'] = df['tkt_3f'].apply(lambda x: f"'{x}")
-        df['win_method_逃げ'] = df['win_method'].apply(lambda x: 1 if x == '逃げ' else 0)
-        df['win_method_まくり'] = df['win_method'].apply(lambda x: 1 if x == 'まくり' else 0)
-        df['win_method_まくり差し'] = df['win_method'].apply(lambda x: 1 if x == 'まくり差し' else 0)
-        df['win_method_差し'] = df['win_method'].apply(lambda x: 1 if x == '差し' else 0)
-        df['win_method_抜き'] = df['win_method'].apply(lambda x: 1 if x == '抜き' else 0)
-        df['win_method_恵まれ'] = df['win_method'].apply(lambda x: 1 if x == '恵まれ' else 0)
-        df['weather_晴'] = df['weather'].apply(lambda x: 1 if x == '晴' else 0)
-        df['weather_曇り'] = df['weather'].apply(lambda x: 1 if x == '曇り' else 0)
-        df['weather_雨'] = df['weather'].apply(lambda x: 1 if x == '雨' else 0)
-        df['wind_東'] = df['wind'].apply(lambda x: 1 if x == '東' else 0)
-        df['wind_西'] = df['wind'].apply(lambda x: 1 if x == '西' else 0)
-        df['wind_南'] = df['wind'].apply(lambda x: 1 if x == '南' else 0)
-        df['wind_北'] = df['wind'].apply(lambda x: 1 if x == '北' else 0)
-        df['wind_南東'] = df['wind'].apply(lambda x: 1 if x == '南東' else 0)
-        df['wind_南西'] = df['wind'].apply(lambda x: 1 if x == '南西' else 0)
-        df['wind_北西'] = df['wind'].apply(lambda x: 1 if x == '北西' else 0)
-        df['wind_北東'] = df['wind'].apply(lambda x: 1 if x == '北東' else 0)
-        repl_mapper = {'K': np.nan, '.': np.nan}
-        for i in range(1, 7):
-            df[f'ET_{i}'] = df[f'ET_{i}'].replace(repl_mapper)
-            df[f'ST_{i}'] = df[f'ST_{i}'].replace(repl_mapper)\
-                .str.replace('F', '-').str.replace('L', '1')
-            df[f'RT_{i}'] = df[f'RT_{i}'].map(conv_racetime)
-        waku = np.array([('{}'*6).format(*v) for v in df[
-            [f'SC_{i}' for i in range(1, 7)]].values])
-        df['wakunari'] = np.where(waku == '123456', 1, 0)
-        df = df.replace({'K': np.nan})
-        df.to_csv(f'D:/BoatRace/result/result_2023/result_{date}.csv', index=False, encoding='cp932')
-        return df
-    else:
-        return None
+x_test = X_test[DataList]
+t_test = T_test["win_1"]
+
+print(len(x_train))
+print(len(x_test))
+
+x_train= torch.tensor(x_train.values, dtype=torch.float32)
+x_test= torch.tensor(x_test.values, dtype=torch.float32)
+t_train= torch.tensor(t_train.values, dtype=torch.long)
+t_test= torch.tensor(t_test.values, dtype=torch.long)
+
+t_train = t_train.reshape((-1,1))
+t_test = t_test.reshape((-1,1))
+
+from torch.utils.data import TensorDataset, DataLoader
+train_dataset = TensorDataset(x_train, t_train)
+test_dataset = TensorDataset(x_test, t_test)
+
+batch_size = 430
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=False)
+test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, drop_last=False)
+
+import torch.nn.functional as F
+class model(nn.Module):
+    def __init__(self):
+        super(model, self).__init__()
+        self.model_info = nn.ModuleList([
+             nn.Linear(66,100),
+             nn.ReLU(),
+             nn.BatchNorm1d(100),
+             nn.Dropout(0.5),
+             nn.Linear(100,50),
+             nn.ReLU(),
+             nn.BatchNorm1d(50),
+             nn.Dropout(0.5),
+             nn.Linear(50,1),
+             nn.Sigmoid()
+            ])
+
+    def forward(self, x):
+        for i in range(len(self.model_info)):
+            x = self.model_info[i](x)
+        return x
+
+device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
+model = model().to(device)
+
+from torch import optim
+criterion = nn.BCELoss()
+l1_lambda = 0.0001
+l2_lambda = 0.0001
+optimizer = optim.Adam(model.parameters(), lr = 0.00001, weight_decay = l2_lambda)
+for param in model.parameters():
+    param.register_hook(lambda grad: l1_lambda * torch.sign(grad))
+num_epochs = 500
+
+loss_train_history = []
+loss_test_history = []
+accuracy_train_list = []
+accuracy_test_list = []
+
+for epoch in range(num_epochs):
+  model.train()
+
+  epoch_train_loss = 0
+  epoch_test_loss = 0
+  num_train_batches = 0
+  num_test_batches = 0
+  correct_train = 0
+  correct_test = 0
+
+  for x,t in train_dataloader:
+    x = x.to(device)
+    t = t.to(device).float()
+    optimizer.zero_grad()
+    y = model(x)
+    loss_train = criterion(y,t)
+    epoch_train_loss += loss_train.item()
+    num_train_batches += 1
+    pred_train = torch.where(y < 0.5,0,1)
+    correct_train += pred_train.eq(t.view_as(pred_train)).sum().item()
+    loss_train.backward()
+    optimizer.step()
+
+  model.eval()
+
+  with torch.no_grad():
+    for x,t in test_dataloader:
+      x = x.to(device)
+      t = t.to(device).float()
+      y = model(x)
+      loss_test = criterion(y,t)
+      epoch_test_loss += loss_test.item()
+      num_test_batches += 1
+      pred_test = torch.where(y < 0.5,0,1)
+      correct_test += pred_test.eq(t.view_as(pred_test)).sum().item()
+
+  avg_train_loss = epoch_train_loss / num_train_batches
+  loss_train_history.append(avg_train_loss)
+  avg_test_loss = epoch_test_loss / num_test_batches
+  loss_test_history.append(avg_test_loss)
+  avg_train_acc = correct_train / len(train_dataset)
+  accuracy_train_list.append(avg_train_acc)
+  avg_test_acc = correct_test / len(test_dataset)
+  accuracy_test_list.append(avg_test_acc)
 
 
-# 開始日と終了日を指定
-start_date = date(2020, 1, 19)
-end_date = date(2020, 1, 19)
+  print(f"Epoch: {epoch+1}/{num_epochs}, Train_Loss: {avg_train_loss:.4f}, Train_Acc: {avg_train_acc:.4f}, Test Loss: {avg_test_loss:.4f}, Test_Acc: {avg_test_acc:.4f}")
 
-# date_range 関数を使用して日付を生成
-for d in date_range(start_date, end_date):
-    date_str = d.strftime("%Y-%m-%d")
-    download_file('results', date_str)
-    read_file('results', date_str)
-    get_results(date_str)
+model.eval()
+
+loss_sum = 0
+correct = 0
+
+with torch.no_grad():
+  for x,t in test_dataloader:
+    x = x.to(device)
+    t = t.to(device).float()
+    y = model(x)
+    loss_sum += criterion(y,t)
+    pred = torch.where(y < 0.5,0,1)
+    correct += pred.eq(t.view_as(pred)).sum().item()
+
+print(f"Test Loss: {loss_sum.item() / len(test_dataloader)}, {100*correct/len(test_dataset)}% ({correct}/{len(test_dataset)})")
+
+plt.figure(figsize=(10, 6))
+plt.plot(range(1, num_epochs + 1), loss_train_history, label = 'Train Loss')
+plt.plot(range(1, num_epochs + 1), loss_test_history, label = 'Test Loss')
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.grid()
+plt.legend()
+plt.show()
+
+plt.figure(figsize=(10, 6))
+plt.plot(range(1, num_epochs + 1), accuracy_train_list, label='Train Accuracy')
+plt.plot(range(1, num_epochs + 1), accuracy_test_list, label='Test Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.grid()
+plt.legend()
+plt.show()
+
+pred = torch.where(model(x_train.to(device)) < 0.5,0,1)
+
+#pred = pred.detach().cpu().numpy()
+#pred
+
+#prob = model(x_train.to(device))
+#prob = prob.detach().cpu().numpy()
+#prob
+
+#X_train['pred_binary_1'] = pred
+
+#X_train['pred_prob_1'] = prob
+
+#X_train
+
+#data_1 = X_train[X_train['pred_binary_1'] == 1]
+#data_2 = X_train[X_train['pred_binary_1'] == 0]
+
+#data_2[['rank_1']]
+
+#data_1.to_csv('data_逃げるレース_3.csv', index=None, encoding='cp932')
+
+#data_2.to_csv('data_逃げないレース_3.csv', index=None, encoding='cp932')
+
+acc = accuracy_score(t_train,pred.cpu())
+print('Acc :', acc)
+pre = precision_score(t_train, pred.cpu(), average = None)
+print('Pre :', pre)
+recall = recall_score(t_train, pred.cpu(), average = None)
+print('Recall :', recall)
+f1 = f1_score(t_train, pred.cpu(), average=None)
+print('F1 :', f1)
+
+pred_1 = torch.where(model(x_test.to(device)) < 0.5,0,1)
+
+acc = accuracy_score(t_test,pred_1.cpu())
+print('Acc :', acc)
+pre = precision_score(t_test, pred_1.cpu(), average = None)
+print('Pre :', pre)
+recall = recall_score(t_test, pred_1.cpu(), average = None)
+print('Recall :', recall)
+f1 = f1_score(t_test, pred_1.cpu(), average=None)
+print('F1 :', f1)
+
+print(loss)
+
+from sklearn.metrics import accuracy_score
+accuracy_score(t_test, model(x_test).argmax(dim=1))
+
+! pip install shap
+
+import shap
+
+explanier = shap.DeepExplainer(model, x_train.to(device))
+
+shap_values = explanier.shap_values(x_train.to(device), t_train.to(device))
+
